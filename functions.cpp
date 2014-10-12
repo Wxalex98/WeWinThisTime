1a790311a5bee6fad22183e3c1cb2dd645c885ff
RicardoDominguez <riddoddo@gmail.com>
2014-10-12 10:15:28 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,18 @@
 //float function for getting the distance between two points
 float vector[3];
 float dist;
+
+bool areWeinsideShadow(float pos[3]){
+    centerPos[0] = pos[0];
+    distance2center = getDistance(centerPos, pos);
+    
+    if (pos[0] > 0 && distance2center < asteroidRadius) {
+       return true;
+    } else {
+        return false;
+    }
+}
+
 float getDistance(float pos[3],float target[3]){
     mathVecSubtract(vector,target,pos,3);
     dist = mathVecMagnitude(vector,3);
