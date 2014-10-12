16f7980b4d8f48acbbca91e8669f876d8533aba7
RicardoDominguez <riddoddo@gmail.com>
2014-10-12 12:34:08 +0200

    Update goToShadow.cpp

--- goToShadow.cpp
+++ goToShadow.cpp
@@ -1,3 +1,19 @@
+float ourState[12];
+float ourPos[3];
+float wayP[3][3];
+ 
+void init(){
+   
+    // give values to all waypoints
+    wayP[0][0] = ;
+    wayP[0][0] = ;
+    wayP[0][0] = ;
+    
+    wayP[0][0] = ;
+    wayP[0][0] = ;
+}
+ 
+
 void goToShadow(float ourPos[3]) {
         if (ourPos[0] > 0 && ourPos[0] < (asteroidRadius + sphereRadius + 0.1f)) {
             	centerPos[0] = asteroidRadius + sphereRadius + 0.1f;
